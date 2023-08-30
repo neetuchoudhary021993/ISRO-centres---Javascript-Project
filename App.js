ff9f5edbaa5b7cdc38c442dbb20d59c34920f05d
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-08-30 22:08:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -8,26 +8,59 @@ const search_input = document.getElementById("search_input");
 let selectedBtn = centerBtn;
 let centresArr = [];
 
+//use variable to store api_URL
+
 const result_table = document.getElementById("result");
 
 async function fetch_ISRO_data() {
-    try {
-        const response = await fetch("https://isro.vercel.app/api/centres");
+  try {
+    const response = await fetch("https://isro.vercel.app/api/centres");
 
-        const result = await response.json();
-        centresArr = result.centres;
-        console.log("fetching centres:", centresArr);
-        // console.log(result)
-        render_ISRO_data(centresArr);
-        // print(centresArr);
-    } catch (error) {
-        console.error(error);
-    }
+    const result = await response.json();
+    centresArr = result.centres;
+    console.log("fetching centres:", centresArr);
+    // console.log(result)
+    render_ISRO_data(centresArr);
+    // print(centresArr);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 fetch_ISRO_data(centresArr);
 
 // console.log("centreArr", centresArr);
+
+// function render_ISRO_data(centresArr) {
+//   //   console.log(centres)
+//   result_table.innerHTML = "";
+
+//   centresArr.map((centre) => {
+//     const { name, Place, State } = centre;
+
+//     let table = document.createElement("table");
+
+//     table.className = "card_container";
+
+//     table.innerHTML += `
+//       <thead class="table-header">
+//         <tr class="table-header-row">
+//             <th>CENTER</th>
+//             <th>CITY</th>
+//             <th>STATE</th>
+//         </tr>
+//        </thead>
+//        <tbody class="table-body">
+//         <tr class="table-body-row">
+//             <td class="table-body-cell center">${name}</td>
+//             <td class="table-body-cell city">${Place}</td>
+//             <td class="table-body-cell state">${State}</td>
+//         </tr>
+//        </tbody>
+//        `;
+//     result_table.appendChild(table);
+//   });
+// }
 
 function render_ISRO_data(centresArr) {
     //   console.log(centres)
@@ -84,70 +117,89 @@ btns = btns.filter((btn) => btn.id != "search_btn");
 // console.log(selectedBtn);
 
 btns.forEach((btn) => {
-    btn.addEventListener("click", activeClass);
+  btn.addEventListener("click", activeClass);
 });
 
+// which btn has invoked this function
 function activeClass(e) {
-    // e.target.classList.toggle("active");
-    console.log(e.target);
-    btns.forEach((btn) => {
-        // console.log(btn);
-        if (btn == e.target) {
-            e.target.classList.toggle("active");
-            if (btn.classList.contains("active")) {
-                selectedBtn = btn;
-            } else {
-                selectedBtn = centerBtn;
-                render_ISRO_data(centresArr);
-            }
-        } else {
-            btn.classList.remove("active");
-        }
-        console.log(btn);
-    });
-    console.log("selected", selectedBtn);
+  // e.target.classList.toggle("active");
+  console.log(e.target);
+  btns.forEach((btn) => {
+    console.log(btn);
+    // use switch conditiion if cond is city then selectedBtn is city
+    if (btn == e.target) {
+      e.target.classList.toggle("active");
+      if (btn.classList.contains("active")) {
+        selectedBtn = btn;
+      } else {
+        selectedBtn = centerBtn;
+        render_ISRO_data(centresArr);
+      }
+    } else {
+      btn.classList.remove("active");
+    }
+    console.log(btn);
+  });
+  console.log("selected", selectedBtn);
 }
 
+//modifying an array is expensive task
+
 searchBtn.addEventListener("click", (e) => {
-    let search = search_input.value.toLowerCase();
-    console.log(search);
-    console.log(selectedBtn.id);
-    // city == Place
-    // state == State
-    // centre == name
-    let key;
-    if (selectedBtn.id == "city") {
-        key = "Place";
-    } else if (selectedBtn.id == "state") {
-        key = "State";
-    } else {
-        key = "name";
-    }
-    console.log(key);
-    let arr = centresArr.filter((center) => center[key].toLowerCase() == search);
+  let search = search_input.value.toLowerCase();
+  console.log(search);
+  console.log(selectedBtn.id);
+  // city == Place
+  // state == State
+  // centre == name
+  let key;
+  if (selectedBtn.id == "city") {
+    key = "Place";
+  } else if (selectedBtn.id == "state") {
+    key = "State";
+  } else {
+    key = "name";
+  }
+  console.log(key);
+  let arr = centresArr.filter((center) => center[key].toLowerCase() == search);
+  if(arr.length==0) {
+    result_table.innerHTML = `
+    <p class="result-not-found">result not found!</p>`
+  }
+  else{
     console.log(arr);
     render_ISRO_data(arr);
+  }
+  // render_ISRO_data(arr);
 });
 
 search_input.addEventListener("keyup", (e) => {
-    // console.log("target", e.target.value);
-    let search = search_input.value.toLowerCase();
-    console.log("search", search);
+  // console.log("target", e.target.value);
+  let search = search_input.value.toLowerCase();
+  console.log("search", search);
 
-    let key;
-    if (selectedBtn.id == "city") {
-        key = "Place";
-    } else if (selectedBtn.id == "state") {
-        key = "State";
-    } else {
-        key = "name";
-    }
+  let key;
+  if (selectedBtn.id == "city") {
+    key = "Place";
+  } else if (selectedBtn.id == "state") {
+    key = "State";
+  } else {
+    key = "name";
+  }
 
-    let arr = centresArr.filter((center) => {
-        // return center[key].toLowerCase() == (e.target.value).toLowerCase();
-        return center[key].toLowerCase().includes(search);
-    });
+  let arr = centresArr.filter((center) => {
+    // return center[key].toLowerCase() == (e.target.value).toLowerCase();
+    return center[key].toLowerCase().includes(search);
+  });
+
+  if(arr.length == 0) {
+    result_table.innerHTML = `
+    <p class="result-not-found">result not found!</p>`
+  }
+  else {
     console.log(arr);
     render_ISRO_data(arr);
+  }
+  // render_ISRO_data(arr);
 });
 
